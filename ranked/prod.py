71cbffe717becaa92bf7ccbd1ec1ba5023344e1a
gregmurray <gd-murray@hotmail.com>
2019-03-15 19:43:27 -0400

    dj-database

--- ranked/prod.py
+++ ranked/prod.py
@@ -1,5 +1,6 @@
 from .settings import *
 
+import dj_database_url
 ALLOWED_HOSTS = [
     "ranked-gg.herokuapp.com",
     "ranked.wtf"
@@ -8,3 +9,5 @@ ALLOWED_HOSTS = [
 CORS_ORIGIN_WHITELIST = (
     'ranked.wtf'
 )
+
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
