aeda24e4c984a5c4910849dce28123f6cf037cae
gregmurray <gd-murray@hotmail.com>
2019-03-15 19:58:13 -0400

    remove prod stuff

--- ranked/prod.py
+++ ranked/prod.py
@@ -1,7 +1,5 @@
 from .settings import *
 
-import dj_database_url
-
 ALLOWED_HOSTS = [
     "ranked-gg.herokuapp.com",
     "ranked.wtf"
@@ -10,6 +8,4 @@ ALLOWED_HOSTS = [
 CORS_ORIGIN_WHITELIST = (
     'ranked.wtf'
 )
-print(os.environ.get('DATABASE_URL'))
-DATABASES['default'] = dj_database_url.config()
-# DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
+
